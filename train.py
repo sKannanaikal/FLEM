07c6542164174f62bcfbb6b0c6c493f78619f3ba
Sean Kannanaikal <stk5106@g.rit.edu>
2024-10-23 07:02:30 -0400

    Update and rename flem.py to train.py

--- train.py
+++ train.py
@@ -5,6 +5,7 @@
 @author Sean Kannanaikal (stk5106)
 '''
 
+import json
 import math
 import torch
 import os
@@ -17,20 +18,22 @@ import torchvision
 import torchvision.transforms as transforms
 import pandas as pd
 from torch.nn.utils.rnn import pad_sequence
-
+import lief
 import pe_sections
-
-
-
 from datetime import datetime
+import warnings
 
-GLOBAL_BENIGN_EXECUTABLES_LOCATION = "/home/stk5106/raw_byte_classifier/dataset/benign/" #folder containing benign PEs
-GLOBAL_MALICIOUS_EXECUTABLES_LOCATION = "/home/stk5106/raw_byte_classifier/dataset/malware" #folder containing malicious PEs
-GLOBAL_MAXIMUM_EXECUTABLE_SIZE = 100000
-GLOBAL_LOG_FILE = '/home/stk5106/raw_byte_classifier/outputLog'
+GLOBAL_BENIGN_EXECUTABLES_LOCATION = "/home/stk5106/raw_byte_classifier/dataset/benign/"
+GLOBAL_MALICIOUS_EXECUTABLES_LOCATION = "/home/stk5106/raw_byte_classifier/dataset/malware/"
+GLOBAL_MAXIMUM_EXECUTABLE_SIZE = 1000000
+GLOBAL_WHOLE_EXE_FLAG = 0 #0 if you only want to look at code 1 if you want to look at whole exe
+GLOBAL_EXE_MAP = '/home/stk5106/raw_byte_classifier/exe_map_updated.json'
+GLOBAL_EXECUTABLE_SECTIONS_FILE = '/home/stk5106/raw_byte_classifier/executableSections.json'
+GLOBAL_EXECUTABLE_SECTIONS_BOUNDS = []
+GLOBAL_TRAINING_RESULTS = []
+GLOBAL_VALIDATION_RESULTS = []
 
 class MalConvConfig:
-
     """
     Configuration used by original authors:
 
@@ -156,25 +159,23 @@ class ExecutableDataset(Dataset):
         return len(self.dataframe.index)
 
     def __getitem__(self, index):
-        global GLOBAL_BENIGN_EXECUTABLES_LOCATION
-        global GLOBAL_MALICIOUS_EXECUTABLES_LOCATION
-
+        fake = 0
         filename = ''
+        
         benignSamples = os.listdir(GLOBAL_BENIGN_EXECUTABLES_LOCATION)
         maliciousSamples = os.listdir(GLOBAL_MALICIOUS_EXECUTABLES_LOCATION)
-        #update these range numbers
+        
         if(index >= 0 and index <= len(benignSamples) - 1):
-            
-            filename = f'/benign/{benignSamples[index]}'
+            filename = f'benign/{benignSamples[index]}'
 
         elif(index >= len(benignSamples) and index <= len(benignSamples) + len(maliciousSamples)):
-            filename = f'/malware/{maliciousSamples[index - len(benignSamples)]}'
-
+            filename = f'malware/{maliciousSamples[index - len(benignSamples)]}'
+        
         else:
             print('[-] Invalid Index Requested')
             exit(0)
         
-        filepath = f'/home/stk5106/raw_byte_classifier/dataset{filename}'
+        filepath = f'/home/stk5106/raw_byte_classifier/dataset/{filename}'
         binaryDataVectorized, fake = readBinary(filepath)
         
         if fake == 1:
@@ -189,33 +190,65 @@ def custom_collate(batch):
     paddedBinaryVectors = pad_sequence(binaryVectors, batch_first=True, padding_value=257)
     return paddedBinaryVectors, maliciousLabels
 
+'''
+TODO this is a prototype for the new readBinary for parsing the json file if we integrate luke's code
+'''
+def readBinaryExecutableMap(path):
+    filename = path.split('/')[-1]
+    binaryDataRaw = b''
+    fake = 0
+    sectionOffsets = GLOBAL_EXECUTABLE_SECTIONS_BOUNDS[filename]["bounds"]
+    for section in sectionOffsets:
+        startOffset = section[0]
+        endOffset = section[1]
+        length = endOffset - startOffset
+        with open(path, 'rb') as malwareSample:
+            malwareSample.seek(startOffset)
+            binaryDataRaw += malwareSample.read(length)
+    
+    if(binaryDataRaw == b''):
+        binaryDataRaw = b'\x00'
+        fake = 1
+
+    binaryDataVector = torch.frombuffer(binaryDataRaw[:GLOBAL_MAXIMUM_EXECUTABLE_SIZE], dtype=torch.uint8)
+    binaryDataVector = binaryDataVector.to(torch.int64)
+    
+    return binaryDataVector, fake
+
+'''
+TODO might want to update this function during the processing stage with Luke's new code i'd say
+'''
 def readBinary(path):
     global GLOBAL_MAXIMUM_EXECUTABLE_SIZE
+    global GLOBAL_WHOLE_EXE_FLAG #modify this value from 0 to 1 depending on which style of input data you want to give
+    
     binaryDataRaw = b''
     fake = 0
     with open(path, "rb") as executable:
+        if (GLOBAL_WHOLE_EXE_FLAG == 0):
+            lief.logging.disable()
+            extractor = pe_sections.GetExecutableSectionBounds(path) #calling Luke's PE sections function
+            bounds, error = extractor('lief')
         
-        extractor = pe_sections.GetExecutableSectionBounds(path)
-        bounds, error = extractor('lief')
-        
-        for sectionByteRange in bounds:
-            length = sectionByteRange[1] - sectionByteRange[0]
-            if length > 0 and sectionByteRange[0] >= 0:
-                executable.seek(sectionByteRange[0])
-                binaryDataRaw += bytearray(executable.read(length))
-
-        #binaryDataRaw = bytearray(executable.read(GLOBAL_MAXIMUM_EXECUTABLE_SIZE))
+            for sectionByteRange in bounds:
+                length = sectionByteRange[1] - sectionByteRange[0]
+                if length > 0 and sectionByteRange[0] >= 0:
+                    executable.seek(sectionByteRange[0])
+                    binaryDataRaw += bytearray(executable.read(length))
+        else:
+            binaryDataRaw = bytearray(executable.read(GLOBAL_MAXIMUM_EXECUTABLE_SIZE))
         
         if(binaryDataRaw == b''):
-            binaryDataRaw = b'\x00'
+            binaryDataRaw = b'\x00'*GLOBAL_MAXIMUM_EXECUTABLE_SIZE
             fake = 1
             
         binaryDataVector = torch.frombuffer(binaryDataRaw[:GLOBAL_MAXIMUM_EXECUTABLE_SIZE], dtype=torch.uint8)
         binaryDataVector = binaryDataVector.to(torch.int64)
+            
         return binaryDataVector, fake
-        
-    return b''
+
 def validate(model, validationLoader, lossFunction, device, datasetSize):
+    global GLOBAL_VALIDATION_RESULTS
     model.eval()
     runningLoss = 0.0
     correct = 0
@@ -225,25 +258,31 @@ def validate(model, validationLoader, lossFunction, device, datasetSize):
             outputs = model(images)
             loss = lossFunction(outputs, labels)
             runningLoss += loss.item() * labels.size(0)
+            print(f'[+] Running Loss: {runningLoss}')
             for i in range(len(outputs)):
                 correct += checkPrediction(outputs[i], labels[i])
-            
     accuracy = 100 * correct / len(validationLoader.dataset) 
-    print(f'Model Validation Accuracy: {accuracy}%')
-
+    GLOBAL_VALIDATION_RESULTS.append(accuracy)
+    print(f'[+] Model Validation Accuracy: {accuracy}%')
     validationLoss = runningLoss / len(validationLoader.dataset)
+    print(f'[+] Training Loss: {validationLoss} Final Running Loss: {runningLoss} Size of Dataset: {len(validationLoader.dataset)}')
     return validationLoss
 
+'''
+maybe with new check predictions stuff it will be beteter
+'''
 def checkPrediction(logit, label):
     predicted_labels = torch.argmax(logit, dim=0)
-    
     if predicted_labels.item() == label.item():
+        print('[+] Correct')
         return 1
-    
+    print('[-] Incorrect')
     return 0
     
 
 def train(model, trainingLoader, lossFunction, optimizer, device, datasetSize):
+    global GLOBAL_TRAINING_RESULTS
+    
     model.train()
     runningLoss = 0.0
     correct = 0
@@ -255,21 +294,27 @@ def train(model, trainingLoader, lossFunction, optimizer, device, datasetSize):
         loss.backward()
         optimizer.step()
         runningLoss += loss.item() * labels.size(0)
+        print(f'[+] Running Loss: {runningLoss}')
         for i in range(len(outputs)):
             correct += checkPrediction(outputs[i], labels[i])
-    
     accuracy = 100 * correct / len(trainingLoader.dataset) 
-    
-    global GLOBAL_LOG_FILE
-    with open(GLOBAL_LOG_FILE) as log:
-        log.write(f'Model Training Accuracy: {accuracy}%')
-        
+    print(f'[+] Model Training Accuracy: {accuracy}%') 
+    GLOBAL_TRAINING_RESULTS.append(accuracy)
     trainingLoss = runningLoss / len(trainingLoader.dataset)
+    print(f'[+] Training Loss: {trainingLoss} Final Running Loss: {runningLoss} Size of Dataset: {len(trainingLoader.dataset)}')
     return trainingLoss
 
 def main():
+    global GLOBAL_EXECUTABLE_SECTIONS_BOUNDS
+    
+    warnings.filterwarnings("ignore", category=UserWarning) 
+    
     dataDirectory = '/home/stk5106/raw_byte_classifier/dataset/'
     labels = '/home/stk5106/raw_byte_classifier/dataset/labels.csv'
+    
+    with open(GLOBAL_EXECUTABLE_SECTIONS_FILE, 'r') as jsonData:
+        GLOBAL_EXECUTABLE_SECTIONS_BOUNDS = json.load(jsonData)
+    
     dataset = ExecutableDataset(labels, dataDirectory, None)
 
     datasetLength = dataset.__len__()
@@ -277,12 +322,13 @@ def main():
     trainingSetSize = int(0.8 * datasetLength)
     validationSetSize = datasetLength - trainingSetSize
 
+    #TODO revert the sizes back to normal
     trainingDataset, validationDataset = torch.utils.data.random_split(dataset, [trainingSetSize, validationSetSize])
     
     print('[+] Split Dataset into training and validation set!')
 
-    trainingLoader = DataLoader(trainingDataset, batch_size=20, shuffle=True, collate_fn=custom_collate)
-    validationLoader = DataLoader(validationDataset, batch_size=20, shuffle=True, collate_fn=custom_collate)
+    trainingLoader = DataLoader(trainingDataset, batch_size=1, shuffle=True, collate_fn=custom_collate)
+    validationLoader = DataLoader(validationDataset, batch_size=1, shuffle=True, collate_fn=custom_collate)
     
     print('[+] Created loaders for each dataset')
 
@@ -290,13 +336,14 @@ def main():
 	
     configuration = MalConvConfig(num_labels=2, pad_token_id=257)
     model = MalConvForSequenceClassification(configuration)
+    
     model.to(device)
     
     print('[+] Moved Model Over to GPU')
 
     #TODO: look more into the loss and optimizer function
     lossFunction = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, momentum=0.9)
 
     epochCount = 5
 
@@ -306,22 +353,24 @@ def main():
         print(f'[+] Starting Training Sequence {epoch}')
         trainingLoss = train(model, trainingLoader, lossFunction, optimizer, device, trainingSetSize)
         trainingLosses.append(trainingLoss)
-
+        
+        print(f"[+] Epoch {epoch + 1}/{epochCount} - Training Loss: {trainingLoss}")
+        
         print(f'[+] Starting Validation Sequence {epoch}')
         validationLoss = validate(model, validationLoader, lossFunction, device, validationSetSize)
         validationLosses.append(validationLoss)
-
-        global GLOBAL_LOG_FILE
-        with open(GLOBAL_LOG_FILE) as log:
-            log.write(f"Epoch {epoch + 1}/{epochCount} - Training Loss: {trainingLoss}, Validation Loss: {validationLoss}")
         
+        print(f"[+] Epoch {epoch + 1}/{epochCount} - Validation Loss: {validationLoss}\n")
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
         
-        model_path = f"/home/stk5106/raw_byte_classifier/models/modelCodeOnlySunday_{timestamp}_{epoch}"
+        model_path = f"/home/stk5106/raw_byte_classifier/models/flem_{timestamp}_{epoch}"
         
         torch.save(model.state_dict(), model_path)
         
         print('[+] Model Saved')
+    
+    print(f'Training Accuracies: {GLOBAL_TRAINING_RESULTS}\nTraining Losses: {trainingLosses}')
+    print(f'Validation Accuracies: {GLOBAL_VALIDATION_RESULTS}\nValidation Losses: {validationLosses}')
 
 if __name__ == "__main__":
     main()
